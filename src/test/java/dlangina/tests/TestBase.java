4f6ce9074cb318755cab6c98d71d017053506bf3
DariaLangina <dlangina@rvision.ru>
2021-12-18 14:38:38 +0300

    added tests, close cookies in beforeAll

--- src/test/java/dlangina/tests/TestBase.java
+++ src/test/java/dlangina/tests/TestBase.java
@@ -1,5 +1,7 @@
 package dlangina.tests;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
 
 import com.codeborne.selenide.Configuration;
@@ -15,12 +17,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
 
   @BeforeAll
+  public static void start() {
+    config();
+    closeWeUseCookieModal();
+  }
+
   public static void config() {
     SelenoidConfig selenoid = ConfigFactory.create(SelenoidConfig.class);
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     Configuration.startMaximized = true;
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     Configuration.remote = format("https://%s:%s@%s", selenoid.login(),
                                   selenoid.password(), System.getProperty("selenoidUrl")
                                  );
@@ -29,6 +36,11 @@ public class TestBase {
     capabilities.setCapability("enableVNC", true);
     capabilities.setCapability("enableVideo", true);
     Configuration.browserCapabilities = capabilities;
+  }
+
+  public static void closeWeUseCookieModal() {
+    open("https://new.rvision.ru/");
+    $(".t-modal__close-cookie").click();
   }
 
   @AfterEach
